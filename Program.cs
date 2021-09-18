c152701be1b3da154bbe8b6366d024471514d871
XanderChua <72645612+XanderChua@users.noreply.github.com>
2021-09-18 13:17:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class School
+    {
+        private string schoolName, schoolAddress;
+
+
+        public School(string name, string address)
+        {
+            this.schoolName = name;
+            this.schoolAddress = address;
+        }
+        public void printAll()
+        {
+            Console.WriteLine("School Name: " + schoolName);
+            Console.WriteLine("School Address: " + schoolAddress);
+        }
+
+
+        class Student : School
+        {
+            private string studentName, studentAddress;
+            protected int studentID;
+            protected char grades;
+            public Student(string name, string address, int id, char grade, string sname, string saddress) : base(sname, saddress)
+            {
+                this.studentName = name;
+                this.studentAddress = address;
+                this.studentID = id;
+                this.grades = grade;
+            }
+            public void printAll()
+            {
+                base.printAll();
+                Console.WriteLine("Student Name: " + studentName);
+                Console.WriteLine("Student Address: " + studentAddress);
+                Console.WriteLine("Student ID: " + studentID);
+                Console.WriteLine("Student Grades: " + grades);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student("John", "abc123", 001, 'A', "Lala High Sch", "cba321");
+            stud1.printAll();
+            Console.ReadLine();
+        }
+    }
+}
